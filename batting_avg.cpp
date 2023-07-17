8a707edbe760062a8c2bc683aaf0cda23b2611ea
Hitesh <hiteshbhoot938@gmail.com>
2023-07-17 19:06:56 +0530

    CPP

---
+++ batting_avg.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+class batsman{
+
+    int bcode;
+    char bname[20];
+    int innings,notout,runs;
+
+    public:
+
+    int readdata(){
+
+        cout<<"Enter bcode ;";
+        cin>>bcode;
+
+        cout<<"Enter name :";
+        cin>>bname;
+
+        cout<<"Enter innings :";
+        cin>>innings;
+
+        cout<<"Enter No of times batsman remain notout :";
+        cin>>notout;
+
+        cout<<"Enter total scored runs : ";
+        cin>>runs;
+
+    }
+    int calcavg(){
+        int avg=runs/(innings-notout);
+
+        return avg;
+    }
+    int dispalydata(){
+        cout<<"Bcode\tBname\tinnings\tnotout\truns\tbatavg"<<"\n";
+        cout<<bcode<<"\t"<<bname<<"\t"<<innings<<"\t"<<notout<<"\t"<<runs<<"\t"<<calcavg();
+    }
+
+
+};
+
+int main(){
+
+    batsman a;
+    a.readdata();
+    int batavg= a.calcavg();
+     a.dispalydata();
+}
