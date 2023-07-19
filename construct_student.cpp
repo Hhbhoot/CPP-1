c7fbf52ecce9074cd7e4383d383a5895217dc650
Hitesh <hiteshbhoot938@gmail.com>
2023-07-19 18:16:07 +0530

    Constructor

---
+++ construct_student.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class student {
+    int adno;
+    char name[20];
+    float eng, math , guj ;
+    float total ;
+
+    public:
+
+    student(){
+
+        cout<<"Enter ad no :";
+        cin>>adno;
+
+        cout<<"Enter name :";
+        cin>>name;
+
+        cout<<"Enter marks of eng math guj :";
+        cin>>eng>>math>>guj;
+
+        total = eng + math +guj ;
+        
+
+    }  
+    int display(){
+
+        cout<<"ADMNO\tName\tEnglish\tMaths\tGujarati\ttotal :"<<endl;
+        cout<<adno<<"\t"<<name<<"\t"<<eng<<"\t"<<math<<"\t"<<guj<<"\t\t"<<total;
+
+        
+    }
+     
+
+};
+int main(){
+
+    student t ;
+    t.display();
+     
+
+    
+}
