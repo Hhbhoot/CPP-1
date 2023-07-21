41bbf44fe0d6ce8a29d8e9bfaef3d03871d77fce
Hitesh <hiteshbhoot938@gmail.com>
2023-07-21 18:05:08 +0530

    Function oveloading

--- function_overloading_2.cpp
+++ function_overloading_2.cpp
@@ -1,22 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class volume{
-    public:
-
-    void shape(int l){
-        cout<<"volume of cube :"<<l*l*l<<endl;
+class volume
+{
+public:
+    void shape(int l)
+    {
+        cout << "volume of cube :" << l * l * l << endl;
     }
-    void shape(int r ,int h){
-        cout<<"volume of sphere :"<<(4*3.14*r*r*h)/3<<endl;
+    void shape(int r, int h)
+    {
+        cout << "volume of sphere :" << (4 * 3.14 * r * r * h) / 3 << endl;
     }
-    void shape(double r,double h){
-        cout<<"volume of cylinders :"<<(3.114*r*r*h)<<endl;
+    void shape(double r, double h)
+    {
+        cout << "volume of cylinders :" << (3.114 * r * r * h) << endl;
     }
-
 };
-int main(){
+int main()
+{
     volume v;
     v.shape(5);
-    v.shape(5,7);
-    v.shape(2.3,5.0);
+    v.shape(5, 7);
+    v.shape(2.3, 5.0);
 }
--- functionoverloading_1.cpp
+++ functionoverloading_1.cpp
@@ -1,22 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class area{
-    public:
-
-    void shape(int l){
-        cout<<"Area of square :"<<l*l<<endl;
+class area
+{
+public:
+    void shape(int l)
+    {
+        cout << "Area of square :" << l * l << endl;
     }
-    void shape(int l ,int b){
-        cout<<"Area of rectangle :"<<l*b<<endl;
+    void shape(int l, int b)
+    {
+        cout << "Area of rectangle :" << l * b << endl;
     }
-    void shape(double b,double h){
-        cout<<"Area of Triangle :"<<(b*h)/2<<endl;
+    void shape(double b, double h)
+    {
+        cout << "Area of Triangle :" << (b * h) / 2 << endl;
     }
-
 };
-int main(){
+int main()
+{
     area a;
     a.shape(5);
-    a.shape(5,7);
-    a.shape(2.3,5.0);
+    a.shape(5, 7);
+    a.shape(2.3, 5.0);
 }
