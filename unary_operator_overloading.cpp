129940e21faa0641a0ed416f1db951adc10c487b
Hitesh <hiteshbhoot938@gmail.com>
2023-07-24 17:48:39 +0530

    Unary_operator_overloading

---
+++ unary_operator_overloading.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+class number
+{
+    int real, img;
+
+public:
+    void setdata(int a, int b)
+    {
+        real = a;
+        img = b;
+    }
+    void getdata()
+    {
+        cout << "The number is " << real << " + " << img << "i" << endl;
+    }
+
+    number operator++()
+    {
+        number x;
+        x.real = ++real;
+        x.img = ++img;
+        return x;
+    }
+    number operator++(int)
+    {
+        number y;
+        y.real = real++;
+        y.img = img++;
+        return y;
+    }
+};
+int main()
+{
+
+    number t, q, r;
+    t.setdata(5, 1);
+    t.getdata();
+
+    q = ++t;
+
+    q.getdata();
+
+    q++;
+    r = q;
+
+    r.getdata();
+}
